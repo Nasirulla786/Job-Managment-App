884d2a73c7fd55d0c25b63508e55e11a6376e3a4
nasirulla <rehmanansirulla1124@gmail.com>
2026-06-07 19:17:41 +0530

    chnage-3

--- src/app/api/search/suggestions/route.ts
+++ src/app/api/search/suggestions/route.ts
@@ -2,39 +2,33 @@
 import prismaClient from "@/services/primsa ";
 import { NextResponse } from "next/server";
 
-export async function GET(req){
-    const sp = req.nextUrl.searchParams;
-    const q = sp.get("q");
+export async function GET(req) {
+  const sp = req.nextUrl.searchParams;
+  const q = sp.get("q");
 
-    if(!q){
-        return{
-            success:true,
-            suggestions:[]
-        }
-    }
-    const sugg = await prismaClient.openings.findMany({
-        where:{
+  if (!q) {
+    return NextResponse.json({
+      success: true,
+      suggestions: [],
+    });
+  }
 
-            title:{
-                contains:q,
-                mode:"insensitive"
-            }
-        }
-        ,
-        select:{
-            id:true,
-            title:true
-        }
-        ,take :10
-    })
+  const sugg = await prismaClient.openings.findMany({
+    where: {
+      title: {
+        contains: q,
+        mode: "insensitive",
+      },
+    },
+    select: {
+      id: true,
+      title: true,
+    },
+    take: 10,
+  });
 
-return NextResponse.json({
-    success:true,
-    sugg :sugg
-})
-
-
-
-
-
+  return NextResponse.json({
+    success: true,
+    sugg,
+  });
 }
